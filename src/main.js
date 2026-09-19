f6c1ba3cb2f39a64f94ed5cec61ad61e5e31662d
Syed Ali Jawad <jawad.bukhari1511@gmail.com>
2026-09-20 00:39:47 +0500

    add random colors for boxes, added points for tube position vertices, add glow effect

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import spline from './spline.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
 // Create scene
 const scene = new THREE.Scene();
@@ -25,6 +28,24 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.03;
 
+// Post Processing
+const renderScene = new RenderPass(scene,camera);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(
+    window.innerWidth,
+    window.innerHeight
+  ),
+  1.5,
+  0.4,
+  100
+);
+bloomPass.threshold = 0.002;
+bloomPass.strength = 3.5;
+bloomPass.radius = 0;
+const effectComposer = new EffectComposer(renderer);
+effectComposer.addPass(renderScene);
+effectComposer.addPass(bloomPass);
+
 // Create a line from spline
 const points = spline.getPoints(100);
 const geometry = new THREE.BufferGeometry().setFromPoints(points);
@@ -39,14 +60,75 @@ const tubeMaterial = new THREE.MeshBasicMaterial({
   // side: THREE.DoubleSide,
   wireframe: true
 });
-const tube = new THREE.Mesh(tubeGeometry, tubeMaterial);
-// scene.add(tube);
+const tube = new THREE.Mesh(tubeGeometry,tubeMaterial);
 
-// Create edges (dots) from spline
+
+const tubePositions = tubeGeometry.attributes.position;
+const pointsGeometry = new THREE.BufferGeometry();
+pointsGeometry.setAttribute('position',tubePositions);
+const pointsMaterial = new THREE.PointsMaterial({
+  color: 0xffffff,
+  size: 0.025
+});
+const tubePoints = new THREE.Points(pointsGeometry,pointsMaterial);
+scene.add(tubePoints);
+
+
+
+// Create edges (tube lines) from spline
 const edgesGeometry = new THREE.EdgesGeometry(tubeGeometry);
 const edgesMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
-const edges = new THREE.LineSegments(edgesGeometry, edgesMaterial);
-scene.add(edges);
+const tubeLines = new THREE.LineSegments(edgesGeometry, edgesMaterial);
+scene.add(tubeLines);
+
+// Create random boxes inside the tube
+const numBoxes = 100;
+const boxSize = 0.075;
+const boxGeometry = new THREE.BoxGeometry(boxSize,boxSize,boxSize);
+const boxMaterial = new THREE.MeshBasicMaterial({
+  color: 0xffffff,
+  wireframe: true
+});
+
+const randomColors = [
+  0xff0000,
+  0x00ff00,
+  0xffff00,
+  0xff00ff,
+  0x00ffff,
+  0xffffff,
+  0x000000,
+  0x808080,
+];
+
+const boxes = [];
+for (let i = 0; i < numBoxes; i++) {
+  const box = new THREE.Mesh(boxGeometry,boxMaterial);
+
+  // Box random position inside the tube
+  const p = (i / numBoxes + Math.random() * 0.1) % 1;
+  const pos = tubeGeometry.parameters.path.getPointAt(p);
+  pos.x += Math.random() * 0.4;
+  pos.z += Math.random() * 0.4;
+
+  // Box randomrotation inside the tube
+  const rotation = new THREE.Vector3(
+    Math.random() * Math.PI,
+    Math.random() * Math.PI,
+    Math.random() * Math.PI
+  );
+
+  // Convert box geometry into edge lines (hide diagonal lines of the boxes)
+  const boxEdgesGeometry = new THREE.EdgesGeometry(boxGeometry);
+  const boxEdgesMaterial = new THREE.LineBasicMaterial({ color: randomColors[Math.floor(Math.random() * randomColors.length)] });
+  const boxLines = new THREE.LineSegments(boxEdgesGeometry, boxEdgesMaterial);
+
+  boxLines.position.copy(pos);
+  boxLines.rotation.set(rotation.x,rotation.y,rotation.z);
+
+  scene.add(boxLines);
+  // scene.add(box);
+}
 
 function updateCamera(t) {
   const time = t * 0.1;
@@ -69,7 +151,7 @@ window.addEventListener('resize', () => {
 function animate(t = 0) {
   requestAnimationFrame(animate);
   updateCamera(t);
-  renderer.render(scene, camera);
+  effectComposer.render(scene, camera);
   controls.update();
 }
 animate();
